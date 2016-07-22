2933d1045f74408a5e5bb7ef3f65673ab8941a45
DaveC <DaveC@DAVESTABLET>
2016-07-22 15:18:33 -0700

    App has SplitView with working single page GridView showing 5 items of sample data.  Complete with photos.

---
+++ BucketList/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using BucketList.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BucketList
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Item> SampleItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SampleItems = Samples.PopulateSamples();
+        }
+
+        private void Hamburger_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(DisplayIntroListBoxItem.IsSelected) { ResultTextBlock.Text = "Show Introduction here."; } // Navigation goes here
+            else if (DisplayAllListBoxItem.IsSelected) { ResultTextBlock.Text = "All Items display here."; }
+            else if (EditListBoxItem.IsSelected) { ResultTextBlock.Text = "Editing page goes here"; }
+        }
+    }
+}
---
+++ BucketList/Models/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketList.Models
+{
+    public class Item
+    {
+        public int ItemKey { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoMain { get; set; }
+        public int Priority { get; set; }
+    }
+
+    public class Samples
+    {
+        public static ObservableCollection<Item> PopulateSamples()
+        {
+            var samples = new ObservableCollection<Item>();
+            Item itemOne = new Item { ItemKey = 1, Name = "Camping", Description = 
+                "Rent a trailer or camper for a weekend outing.", PhotoMain =
+                "Assets/camper.jpg", Priority = 10 };
+            samples.Add(itemOne);
+            Item itemTwo = new Item { ItemKey = 2, Name = "Play Piano", Description =
+                "Increase piano repetoire with various forms of music.", PhotoMain =
+                "Assets/ElectricKeyboard.jpg", Priority = 1 };
+            samples.Add(itemTwo);
+            Item itemThree = new Item {ItemKey = 3, Name = "Play Violin", Description =
+                "Increase ability to make music with the electric violin.", PhotoMain =
+                "Assets/CecelioViolin.jpg", Priority = 5 };
+            samples.Add(itemThree);
+            Item itemFour = new Item {ItemKey = 4, Name = "Practice French", Description =
+                "Impliment various ways to increase vocabulary and grammar.", PhotoMain =
+                "Assets/France.jpg", Priority = 2 };
+            samples.Add(itemFour);
+            Item itemFive = new Item {ItemKey = 5, Name = "Practice Midi", Description =
+                "Practice midi Software developing as a tool to help master complex musical forms.", PhotoMain =
+                "Assets/MidiMusic.jpg", Priority = 5 };
+            samples.Add(itemFive);
+            return samples;
+        }
+    }
+}
